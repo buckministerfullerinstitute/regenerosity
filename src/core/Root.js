5b98a2f817efba9880deafae5b83adfe481a4c9a
Orestis Herodotou <orestis@digitaltopo.net>
2019-11-02 14:49:28 -0700

    Add material-ui and theme

--- src/core/Root.js
+++ src/core/Root.js
@@ -1,10 +1,16 @@
 import React, {Fragment} from 'react';
 import {hot} from 'react-hot-loader';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
+import theme from './theme';
 import Homepage from '../components/HomePage'
 
 const App = () => <Fragment>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
     <Homepage/>
+    </ThemeProvider>
 </Fragment>;
 
 export default hot(module)(App);
